237f5a00e0788bc4d27f506ac85fe9862c26025d
Arthur <arthur.m.ramos@hotmail.com>
2025-03-07 15:54:35 -0300

    feat: add back button

--- src/app/_components/Header.tsx
+++ src/app/_components/Header.tsx
@@ -1,6 +1,20 @@
+"use client";
+
+import { usePathname, useRouter } from "next/navigation";
+import { Button } from "./ui/button";
+import { MoveLeftIcon } from "lucide-react";
+
 const Header = () => {
+  const router = useRouter();
+  const path = usePathname();
+
   return (
-    <div className="bg-accent-foreground p-4">
+    <div className="bg-accent-foreground p-4 flex items-center gap-4">
+      {path !== "/" && (
+        <Button className="text-secondary" onClick={() => router.back()}>
+          <MoveLeftIcon />
+        </Button>
+      )}
       <h1 className="text-secondary text-2xl font-bold ">Parcerias</h1>
     </div>
   );
